89738c9527b104dad2a530c8c0aa7f9622f5b8c7
Mohsin Khan <mohsink13@gmail.com>
2016-05-12 16:15:26 +0100

    log4net configuration and entity classes

---
+++ src/WebApi2Book.Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Data.Entities {
+    public class User {
+        public virtual long UserId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual byte[] Version { get; set; }
+    }
+}
