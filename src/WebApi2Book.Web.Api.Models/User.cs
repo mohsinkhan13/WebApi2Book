d71caa3b67856cf105b1c167a1608b1cd4a22473
Mohsin Khan <mohsink13@gmail.com>
2016-05-12 17:13:16 +0100

    Added Service model classes

---
+++ src/WebApi2Book.Web.Api.Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Web.Api.Models {
+    public class User {
+        private List<Link> _links;
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public List<Link> Links {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link) {
+            _links.Add(link);
+        }
+    }
+}
